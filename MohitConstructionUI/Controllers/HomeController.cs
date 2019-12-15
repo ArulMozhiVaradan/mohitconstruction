ad67f8eebfb9e062ea7e643a83cf00b6fbcf08c9
ArulMozhiVaradan <jarul@gmail.com>
2019-12-15 16:30:59 +0530

    first commit

---
+++ MohitConstructionUI/Controllers/HomeController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MohitConstructionUI.Models;
+
+namespace MohitConstructionUI.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+
+            ViewBag.ActivePage="home";
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewBag.ActivePage="about";
+            return View();
+        }
+
+        public IActionResult Services()
+        {
+            ViewBag.ActivePage="service";
+            return View();
+        }
+        public IActionResult Project()
+        {
+            ViewBag.ActivePage="project";
+            return View();
+        }
+        public IActionResult Blog()
+        {
+            return View();
+        }
+        public IActionResult BlogDetails()
+        {
+            return View();
+        }
+
+         public IActionResult Contact()
+        {
+            ViewBag.ActivePage="contact";
+            return View();
+        }
+        
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
